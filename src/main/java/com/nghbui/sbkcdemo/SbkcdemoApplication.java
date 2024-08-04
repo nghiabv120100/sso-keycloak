bd0204977d2e073334378b4fca6e5e460e0757b0
nghiabv120100 <nghiabv120100@gmail.com>
2024-08-05 00:18:02 +0700

    commit all

---
+++ src/main/java/com/nghbui/sbkcdemo/SbkcdemoApplication.java
@@ -0,0 +1,13 @@
+package com.nghbui.sbkcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbkcdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbkcdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nghbui/sbkcdemo/SbkcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nghbui.sbkcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbkcdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
